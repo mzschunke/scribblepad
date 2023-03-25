d2166cc218b93cc6cfd9bd608a8545b7bae0dd14
Carl-M. Goersch <goersch@madways.de>
2023-03-25 17:49:24 +0100

    new Content

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const app = document.querySelector('[data-js="app"]');
 
+let color = "#000";
+
 app.style.height = "100vh";
 app.style.display = "flex";
 app.style.alignItems = "center";
@@ -12,16 +14,24 @@ gridContainer.style.height = "50%";
 const inputContainer = document.createElement("div");
 
 const inputField = document.createElement("input");
+inputField.type = "number";
+inputField.addEventListener("change", => {
+    
+})
 
 const colorPicker = document.createElement("input");
+colorPicker.type = "color";
+colorPicker.addEventListener("input", (event) => {
+  color = event.target.value;
+});
 
 const resetButton = document.createElement("button");
+resetButton.type = "button";
+resetButton.textContent = "Reset Grid";
 
 inputContainer.append(inputField, colorPicker, resetButton);
 
 app.append(gridContainer, inputContainer);
-
-let color = "#000";
 
 function Cell() {
   const myCell = document.createElement("div");
@@ -42,4 +52,9 @@ function Grid(value, callback) {
   }
 }
 
-Grid(3, Cell);
+function resetGrid(callback) {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.lastChild);
+  }
+  callback(6, Cell);
+}
