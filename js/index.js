edd985b5ef4b79543a4301a8feab32b77b42ff18
Moritz Zschunke <moritz_z17@web.de>
2023-03-25 16:12:07 +0100

    Created Cell Function

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ gridContainer.style.width = "50%";
 gridContainer.style.height = "50%";
 app.append(gridContainer);
 
+let color = "#000";
+
+
+
+function Cell() {
+    const myCell = document.createElement("div");
+    myCell.setAttribute("data-js", "mycell");
+    myCell.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = color; 
+    }) 
+    return myCell;
+}
+
+const test = Cell();
+console.log(test);
+
